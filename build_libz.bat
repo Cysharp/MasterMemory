1bf7afbfd0ba37472385e7f48cce81e45adba12a
neuecc <ils@neue.cc>
2017-01-09 23:42:30 +0900

    0.1.0 release package

---
+++ build_libz.bat
@@ -0,0 +1,1 @@
+libz inject-dll --assembly src\MasterMemory.CodeGenerator\bin\Release\MasterMemory.CodeGenerator.exe --include src\MasterMemory.CodeGenerator\bin\Release\*.dll --move
---
+++ nuget/push.bat
@@ -0,0 +1,1 @@
+nuget push MasterMemory.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
