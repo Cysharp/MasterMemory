0a15fe63f968f66810f0c8491f714212c6872ca4
neuecc <ils@neue.cc>
2017-03-01 00:03:42 +0900

    0.2.0 done

---
+++ nuget/copy_codegem.bat
@@ -0,0 +1,3 @@
+copy ..\src\MasterMemory.CodeGenerator\bin\Release\*.dll tools\
+copy ..\src\MasterMemory.CodeGenerator\bin\Release\MasterMemory.CodeGenerator.exe tools\MasterMemory.CodeGenerator.exe
+copy ..\src\MasterMemory.CodeGenerator\bin\Release\MasterMemory.CodeGenerator.exe.config tools\MasterMemory.CodeGenerator.exe.config
--- nuget/push.bat
+++ nuget/push.bat
@@ -1,1 +1,1 @@
-nuget push MasterMemory.0.1.0.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push MasterMemory.0.2.0.nupkg -Source https://www.nuget.org/api/v2/package
