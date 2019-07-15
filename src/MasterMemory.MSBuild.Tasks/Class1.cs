4d2580619a2ce63e1565197fe4b88bd4fac9d10a
Yoshifumi Kawai <ils@neue.cc>
2019-07-15 20:44:27 +0900

    MasterMemory.MSBuild.Tasks

--- src/MasterMemory.MSBuild.Tasks/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MasterMemory.MSBuild.Tasks
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/MasterMemory.MSBuild.Tasks/MasterMemoryGenerator.cs
@@ -0,0 +1,31 @@
+﻿using MasterMemory.GeneratorCore;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+
+namespace MasterMemory.MSBuild.Tasks
+{
+    public class MasterMemoryGenerator : Task
+    {
+        [Required]
+        public string UsingNamespace { get; set; }
+        [Required]
+        public string InputDirectory { get; set; }
+        [Required]
+        public string OutputDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                new CodeGenerator().GenerateFile(UsingNamespace, InputDirectory, OutputDirectory, x => this.Log.LogMessage(x));
+            }
+            catch (Exception ex)
+            {
+                this.Log.LogErrorFromException(ex, true);
+                return false;
+            }
+            return true;
+        }
+    }
+}
