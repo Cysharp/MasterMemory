705975a72373c75aefcc0024f07ed9ab9626ad46
neuecc <ils@neue.cc>
2020-03-11 22:40:54 +0900

    generator disable CS0105

--- tests/MasterMemory.Tests/IssueTest.cs
+++ tests/MasterMemory.Tests/IssueTest.cs
@@ -9,23 +9,23 @@ namespace MasterMemory.Tests
 {
     public class IssueTest
     {
-        [Fact]
-        public void Issue49()
-        {
-            var builder = new DatabaseBuilder().Append(new[]
-            {
-                new PersonModel { FirstName = "realname", LastName="reallast" },
-                new PersonModel { FirstName = "fakefirst", LastName="fakelast" },
-            });
+        //[Fact]
+        //public void Issue49()
+        //{
+        //    var builder = new DatabaseBuilder().Append(new[]
+        //    {
+        //        new PersonModel { FirstName = "realname", LastName="reallast" },
+        //        new PersonModel { FirstName = "fakefirst", LastName="fakelast" },
+        //    });
 
-            var data = builder.Build();
-            var database = new MemoryDatabase(data);
+        //    var data = builder.Build();
+        //    var database = new MemoryDatabase(data);
             
-            var entries = database.PersonModelTable.FindClosestByFirstNameAndLastName(("real", "real"), false);
-            var firstEntry = entries.FirstOrDefault();
+        //    var entries = database.PersonModelTable.FindClosestByFirstNameAndLastName(("real", "real"), false);
+        //    var firstEntry = entries.FirstOrDefault();
 
-            var firstIs = firstEntry.FirstName;
+        //    var firstIs = firstEntry.FirstName;
             
-        }
+        //}
     }
 }
