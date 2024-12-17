a45053b74ab2ed6038b09466bff4a149eb2b4f0a
neuecc <ils@neue.cc>
2024-12-17 18:16:21 +0900

    import from generatorcore

--- src/MasterMemory.SourceGenerator/MasterMemoryGenerator.cs
+++ src/MasterMemory.SourceGenerator/MasterMemoryGenerator.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using MasterMemory.GeneratorCore;
+using Microsoft.CodeAnalysis;
 using System;
 
 namespace MasterMemory.SourceGenerator;
@@ -6,8 +7,20 @@ namespace MasterMemory.SourceGenerator;
 [Generator(LanguageNames.CSharp)]
 public partial class MasterMemoryGenerator : IIncrementalGenerator
 {
+    // TODO: property
+    public string? UsingNamespace { get; set; }
+    public string? InputDirectory { get; set; }
+    public string? OutputDirectory { get; set; }
+    public string? PrefixClassName { get; set; }
+    public bool AddImmutableConstructor { get; set; }
+    public bool ReturnNullIfKeyNotFound { get; set; } = true;
+    public bool ForceOverwrite { get; set; }
+
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         // TODO:...
+
+        new CodeGenerator().GenerateFile(UsingNamespace, InputDirectory, OutputDirectory, PrefixClassName, AddImmutableConstructor, !ReturnNullIfKeyNotFound, ForceOverwrite, x => Console.WriteLine(x));
+
     }
 }
