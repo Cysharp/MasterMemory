a1ae56ce29d546e97cd674b9705762f28c63c77c
neuecc <ils@neue.cc>
2024-12-17 18:01:24 +0900

    initial structure

---
+++ src/MasterMemory.SourceGenerator/MasterMemoryGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace MasterMemory.SourceGenerator;
+
+[Generator(LanguageNames.CSharp)]
+public partial class MasterMemoryGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // TODO:...
+    }
+}
---
+++ tests/MasterMemory.SourceGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MasterMemory.SourceGenerator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
